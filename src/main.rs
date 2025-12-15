a7a6fbbeb36f295e3263e0611056d0cc6aed5375
kaydennedyak <kaydenjm133@gmail.com>
2025-12-15 05:23:00 +0000

    Added command: dig, and random chance to get player gold

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
 
-    println!("Welcome to the game!\nWhat is the adventurer's name? ");
+    struct Player {
+        name: String,
+        gold: u16,
+    }
+
+    println!("<> | Welcome to the game!\nWhat is the adventurer's name? ");
 
     let mut name = String::new();
 
@@ -10,9 +16,14 @@ fn main() {
         .read_line( &mut name)
         .expect("Failed to read input");
 
+    let mut player = Player {
+    name: name.trim().to_string(),
+    gold: 0,
+    };
+
     // main gameplay loop
     loop {
-        println!("Greetings, {name}What is your next task?:");
+        println!("<> | Greetings, {}! What is your next task?\n('dig', 'quit') : ", player.name);
 
         let mut input = String::new();
 
@@ -23,11 +34,24 @@ fn main() {
         let input = input.trim();
 
         if input == "quit" {
-            println!("Thank you for playing!");
+            println!("<> | Thank you for playing!");
             break;
         }
+        else if input == "dig" {
+            let mut rng = rand::rng();
+            let chance: u8 = rng.random_range(1..=100);
+
+            if chance <= 40 {
+                let reward = rng.random_range(1..=10);
+                player.gold += reward;
+                println!("You dug and found {} gold! You now have {} total gold.", reward, player.gold);
+               
+            } else {
+                println!("You dug and found nothing!");
+            }
+        }
         else {
-            println!("Not a valid command")
+            println!("<> | Not a valid command")
         }
 
     }
