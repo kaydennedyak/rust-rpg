04788e107345dfb5a4d4a5753687a55ebd39757f
kaydennedyak <kaydenjm133@gmail.com>
2025-12-15 05:06:39 +0000

    basic gameplay loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
 
-    let x = 5;
-    println!("{x}");
+fn main() {
+
+    println!("Welcome to the game!\nWhat is the adventurer's name? ");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line( &mut name)
+        .expect("Failed to read input");
+
+    // main gameplay loop
+    loop {
+        println!("Greetings, {name}What is your next task?:");
+
+        let mut input = String::new();
+
+        io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+        let input = input.trim();
+
+        if input == "quit" {
+            println!("Thank you for playing!");
+            break;
+        }
+        else {
+            println!("Not a valid command")
+        }
+
+    }
 }
